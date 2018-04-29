7140185f408700dfc307715ce4882d99efe54fbd
Evan Genest <genest.1@osu.edu>
2018-04-29 16:32:52 -0500

    Add Troy and Rochester GET routes.

--- js/app.js
+++ js/app.js
@@ -10,5 +10,24 @@ const express = require('express');
 const path = require("path");
 const app = express();
 
-app.set("views", path.resolve(__dirname, "views" )  );
-app.set("view engine", "ejs" );
+app.set('views', path.resolve(__dirname, "views" )  );
+app.set('view engine', 'ejs' );
+
+app.get('/troy', (req, res)=>{
+	console.log("I");
+	res.send("Homer wuz 'ere.");
+	return; // surprise: w/o this, runs 20, 21
+	console.log("II");
+	res.render("header", {
+		message: "Shibolleth."
+	})
+}
+);
+
+app.get('/rochester', (req, res)=>{
+	res.send("If this works, make a view to send.  Then accept a :id arg.");
+
+});
+const PORT = 3001;
+app.listen(PORT);
+console.log(`'m listening on ${PORT}`);
