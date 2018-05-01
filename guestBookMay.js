eecbc457300941d6ba25a49c70939f46af4bca4e
Evan Genest <genest.1@osu.edu>
2018-04-30 19:04:44 -0500

    Type a guest book by rote.  Review of the Bootstrap Evan Hahn Express.js guestbook.

---
+++ guestBookMay.js
@@ -0,0 +1,6 @@
+/*************************           */
+/*  Typing an Evan H. Hahn guestbook */
+/*  based on cold/commenter/app.js   */
+/*  April 30, 2018   @mistergenest   */
+/*************************           */
+
