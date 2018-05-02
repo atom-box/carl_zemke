374607792fdd051d743bd903fe76f409ac9ac82f
Evan Genest <genest.1@osu.edu>
2018-05-02 12:45:19 -0500

    First commit.

--- guestBookMay.js
+++
@@ -1,6 +0,0 @@
-/*************************           */
-/*  Typing an Evan H. Hahn guestbook */
-/*  based on cold/commenter/app.js   */
-/*  April 30, 2018   @mistergenest   */
-/*************************           */
-
