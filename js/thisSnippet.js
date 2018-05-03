337ec23e8fe076d0daf319e1e1c323548534365f
Evan Genest <genest.1@osu.edu>
2018-05-03 18:31:44 -0500

    10 lines of code to try out 'THIS' syntax.  D.N.E. outside of this branch.

---
+++ js/thisSnippet.js
@@ -0,0 +1,22 @@
+// Hope I don't strand this.  It's on the branch!  
+// May 3, 2018
+// Practice THIS syntax, 
+// Zakas page 25, 
+// Passing in a function as an argument
+// TO ANOTHER FUNCTION.  
+
+function sayNameForAll(){
+	console.log(this.name); 
+}
+
+gal1 = {
+	name: 'Veronique',
+	sayName: sayNameForAll
+}
+
+gal2 = {
+	name: 'Nathalie',
+	sayName: sayNameForAll
+}
+
+gal2.sayName();
