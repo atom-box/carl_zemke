0a1d04133226ee14910c2dfd8e8fe7257b669809
Evan Genest <genest.1@osu.edu>
2018-05-03 07:34:45 -0500

    Finish writing the adder function.  Make it accept any number of args and return their sum.

--- js/args-snippet-zakas.js
+++ js/args-snippet-zakas.js
@@ -1,6 +1,16 @@
 function adder(){
 	let i, len, result;
 	// interesting.  This is allowed.
+	result = 0;
+	i = 0;
+	len = arguments.length;
 
+	while(i < len){
+		result += arguments[i];
+		i++;
+	}
+	return result;
 } 
 //huh
+
+console.log(adder(2, 6, 33));
