7b7ce89f9b05540e72556fe18441836ce767b194
Evan Genest <genest.1@osu.edu>
2018-05-05 15:17:42 -0500

    Write a for loop that has '___; x ; x'  syntax.

--- js/roteCloudsArray.js
+++ js/roteCloudsArray.js
@@ -1,16 +1,28 @@
-let clouds = {
-	1: {
+// PRINTS 51,000 lines according to wordcount
+// should add a random cloud generator
+// May 5, 2018 Day after Meg 10 year!
+// Written in 16 minutes, made me feel better.
+let clouds = [
+	 {
 		name: 'cirrus',
 		altitude: 15000
 	},
-	2:{
+	{
 		name: 'cumulus',
 		altitude: 4000
 
 	},
-	3:{
+	{
 		name: 'alto-cumulus',
 		altitude: 32000
 
 	}
-}
+];
+
+clouds.forEach(foo => {
+	let i =0;
+	for ( ; i < foo.altitude; i++ ){
+		console.log(foo.name);
+	}
+
+} );
